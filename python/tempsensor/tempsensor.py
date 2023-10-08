01d0722cf36fb371af016cb5c1872645db0d287a
Mark R. Gilder <gilderm@strose.edu>
2023-10-07 20:16:04 -0400

    Updated to use sqlalchemy engine

--- python/tempsensor/tempsensor.py
+++ python/tempsensor/tempsensor.py
@@ -1,11 +1,11 @@
 import serial
 import time
 from datetime import datetime as dt
-import db as db
-import psycopg2
+from db import connect
+from sqlalchemy import text
 
 # make sure the 'COM#' is set according the Windows Device Manager
-def read_temp_sensor(chemdb):
+def read_temp_sensor(conn):
     ser = serial.Serial('COM3', 9600, timeout=1)
     time.sleep(5)
 
@@ -22,40 +22,20 @@ def read_temp_sensor(chemdb):
             curr_time = dt.now()
             curr_time_str = curr_time.strftime("%Y-%m-%d_%H:%M:%S")
             print(f'{curr_time_str} {flds[0]} {flds[1]} {flds[2]}')
-            insert_id = insert_into_db(chemdb, flds)
+            insert_id = insert_into_db(flds, conn)
             print(f'Inserted sensor data - id {insert_id}')
 
     ser.close()
 
-def insert_into_db(db, flds):
+def insert_into_db(flds, conn=None):
     """ insert a new vendor into the vendors table """
-    sql = """INSERT INTO sensor_data(device_address, sensor_type, sensor_value)
-             VALUES(%s, %s, %s) RETURNING id;"""
-    conn = None
-    id = None
-    try:
-        # read database configuration
-        # params = config()
-        # connect to the PostgreSQL database
-        #conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = db.conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, (flds[0],flds[1],flds[2]))
-        # get the generated id back
-        id = cur.fetchone()[0]
-        # commit the changes to the database
-        db.conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-    return id
+    sql_insert = f"INSERT INTO  sensor_data (device_address, sensor_type, sensor_value) VALUES ('{flds[0]}','{flds[1]}','{flds[2]}' ) RETURNING id"
+    query = text(sql_insert)
+    id = conn.execute(query)
+    conn.commit()
+    return id.fetchone()
 
 if __name__ == "__main__":
-    chemdb = db.DatabaseUtils('localhost','chem','postgres', '1postgres1')
-    read_temp_sensor(chemdb)
+    conn = connect()
+    read_temp_sensor(conn)
+    conn.close()
